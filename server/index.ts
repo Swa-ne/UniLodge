19ff0bfb9c2b8a52d0608f64466bbfba3e2a8be5
swa-ne <spaulbautista@gmail.com>
2024-08-14 23:07:11 +0800

    Add MongoDB connection setup and dotenv configuration to Express server

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,21 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
 
 const port = 3000;
+
+const MONGODB_CONNECTION: any = process.env.MONGODB_CONNECTION;
+mongoose
+    .connect(MONGODB_CONNECTION)
+    .then(() => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('Internal Server Error');
+    });
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from your Node.js Express server!');
