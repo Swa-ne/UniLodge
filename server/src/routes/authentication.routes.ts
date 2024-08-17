900c240634150d095aa024492bdff847d1470911
swa-ne <spaulbautista@gmail.com>
2024-08-17 23:22:02 +0800

    Add authentication middleware to routes and update email code handling logic

--- server/src/routes/authentication.routes.ts
+++ server/src/routes/authentication.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 
 import { resendEmailCodeController, signupUserController, verifyEmailCodeController } from "../controllers/authentication/signup.controller";
+import { authenticateToken } from "../middlewares/token.authentication";
 
 const router = Router();
 
 router.post("/signup", signupUserController);
-router.post("/resend-verification", resendEmailCodeController);
-router.post("/verify-code", verifyEmailCodeController);
+router.post("/resend-verification", authenticateToken, resendEmailCodeController);
+router.post("/verify-code", authenticateToken, verifyEmailCodeController);
 
 export default router;
--- server/src/services/authentication/index.services.ts
+++ server/src/services/authentication/index.services.ts
@@ -1,35 +1,47 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { User, UserValidCode, UserValidCodeSchemaInterface } from '../../models/authentication/user.model';
-import { ObjectId } from 'mongoose';
 import { generateAccessToken, generateRefreshToken } from '../../utils/generate.token';
 import { CustomResponse } from '../../utils/input.validators';
 
-const client = axios.create({
+const python_server = axios.create({
     baseURL: process.env.PYTHON_API_URL,
 });
 
-export const sendEmailCode = async (user_id: ObjectId, receiver: string, name: string) => {
+export const sendEmailCode = async (user_id: string, receiver: string, name: string) => {
     try {
         const code = Math.floor(100000 + Math.random() * 900000);
         const data = { code, receiver, name };
-        await new UserValidCode({
-            user_id,
-            code,
-            expiresAt: new Date(Date.now() + 20 * 60 * 1000)
-        }).save();
-        await client.post(`/send-email-code`, data);
+        const user: UserValidCodeSchemaInterface | null = await UserValidCode.findOne({ user_id });
+        if (user) {
+            user.code = code.toString();
+            user.expiresAt = new Date(Date.now() + 20 * 60 * 1000);
+            await user.save();
+        } else {
+            await new UserValidCode({
+                user_id,
+                code,
+                expiresAt: new Date(Date.now() + 20 * 60 * 1000)
+            }).save();
+        }
+        await python_server.post(`/send-email-code`, data);
     } catch (error) {
         console.error(error);
     }
 }
 
-export const verifyEmailCode = async (user_id: ObjectId, code: string): Promise<CustomResponse> => {
+export const verifyEmailCode = async (user_id: string, code: string): Promise<CustomResponse> => {
     try {
         const user: UserValidCodeSchemaInterface | null = await UserValidCode.findOne({ user_id });
-        if (!user) return { error: "User not found", httpCode: 404 }
+        if (!user) return { error: "User not found", httpCode: 404 };
+        if (user.expiresAt < new Date()) return { error: "Code expired", httpCode: 400 };
         const result: boolean = user.code === code;
-        if (!result) return { error: "Invalid Code", httpCode: 400 }
-        return { message: "Success", httpCode: 200 }
+        if (!result) return { error: "Invalid Code", httpCode: 400 };
+        await UserValidCode.deleteOne({ user_id });
+        await User.findByIdAndUpdate(
+            user_id,
+            { valid_email: true },
+        );
+        return { message: "Success", httpCode: 200 };
     } catch (error) {
         console.error(error);
         return { error: "Internal Server Error", httpCode: 500 }
