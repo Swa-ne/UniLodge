baba6e55b3c247e689dfa2a06c76788408e5db33
swa-ne <spaulbautista@gmail.com>
2024-08-18 22:30:18 +0800

    Refactor login controller to combine input validation and user retrieval services and add authentication routes for signup, verification, and login

--- server/src/controllers/authentication/login.controller.ts
+++ server/src/controllers/authentication/login.controller.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
 
 import { checkEveryInputForLogin } from '../../utils/input.validators';
-import { loginUsertoDatabase } from '../../services/authentication/login.services';
-import { getDataByEmailAddress } from '../../services/authentication/signup.services';
+import { getDataByEmailAddress, loginUsertoDatabase } from '../../services/authentication/login.services';
 import { generateAccessAndRefereshTokens } from '../../services/authentication/index.services';
 
 export const loginUserController = async (req: Request, res: Response) => {
@@ -16,13 +15,6 @@ export const loginUserController = async (req: Request, res: Response) => {
 
             if (data.httpCode === 200) {
                 const user_data = await getDataByEmailAddress(email_address);
-
-                const user = {
-                    email: email_address,
-                    user_id: user_data?._id,
-                    full_name: user_data?.full_name,
-                    username: user_data?.username
-                }
 
                 if (!user_data) {
                     return res.status(404).json({ error: "User not found" });
--- server/src/routes/authentication.routes.ts
+++ server/src/routes/authentication.routes.ts
@@ -3,11 +3,14 @@ import { Router } from "express";
 
 import { resendEmailCodeController, signupUserController, verifyEmailCodeController } from "../controllers/authentication/signup.controller";
 import { authenticateToken } from "../middlewares/token.authentication";
+import { loginUserController } from "../controllers/authentication/login.controller";
 
 const router = Router();
 
 router.post("/signup", signupUserController);
 router.post("/resend-verification", authenticateToken, resendEmailCodeController);
 router.post("/verify-code", authenticateToken, verifyEmailCodeController);
+
+router.post("/login", loginUserController);
 
 export default router;
