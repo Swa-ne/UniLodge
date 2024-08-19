4d60ceb7f573fe14dbcba45e1bd882e700364d35
swa-ne <spaulbautista@gmail.com>
2024-08-19 23:43:36 +0800

    Refactor token generation to validate refresh tokens and handle user data; update access token expiration

--- server/src/utils/generate.token.ts
+++ server/src/utils/generate.token.ts
@@ -1,19 +1,50 @@
 import jwt from 'jsonwebtoken';
 import { getUserRefreshToken } from '../services/authentication/index.services';
+import { User } from '../models/authentication/user.model';
+import { CustomResponse } from './input.validators';
 
-export const generateAccessToken = async (user_id: string, email: string, username: string, full_name: string): Promise<string> => {
-    return jwt.sign(
-        {
-            user_id,
-            email,
-            username,
-            full_name
-        },
-        process.env.ACCESS_TOKEN_SECRET as string,
-        {
-            expiresIn: "30m"
-        }
-    )
+export const generateAccessTokenWithRefreshToken = async (decoded_token: jwt.JwtPayload): Promise<CustomResponse> => {
+    const user = await User.findById(decoded_token.user_id)
+
+    if (!user) return { error: "Invalid refresh token", httpCode: 401 }
+
+    if (decoded_token.refresh_token_version !== (user.refresh_token_version - 1)) return { error: "Refresh token is invalid", httpCode: 401 };
+
+    return {
+        message: jwt.sign(
+            {
+                user_id: user._id,
+                email: user.personal_email,
+                username: user.username,
+                full_name: user.full_name
+            },
+            process.env.ACCESS_TOKEN_SECRET as string,
+            {
+                expiresIn: "30m"
+            }
+        ), httpCode: 200
+    }
+};
+
+export const generateAccessToken = async (user_id: string): Promise<CustomResponse> => {
+    const user = await User.findById(user_id)
+
+    if (!user) return { error: "User Not Found", httpCode: 404 }
+
+    return {
+        message: jwt.sign(
+            {
+                user_id,
+                email: user.personal_email,
+                username: user.username,
+                full_name: user.full_name
+            },
+            process.env.ACCESS_TOKEN_SECRET as string,
+            {
+                expiresIn: "30m"
+            }
+        ), httpCode: 200
+    }
 };
 
 export const generateRefreshToken = async (user_id: string): Promise<string> => {
