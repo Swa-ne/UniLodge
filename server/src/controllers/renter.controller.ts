6c1084c9fc6b21e64e53ea8893fd6a37271a23f3
swa-ne <spaulbautista@gmail.com>
2024-09-05 23:30:56 +0800

    Created validation and error handling for putSavedDorm in the putSavedDormController.

--- server/src/controllers/renter.controller.ts
+++ server/src/controllers/renter.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { UserType } from '../middlewares/token.authentication';
-import { getDorms, postReviewForDorm } from '../services/renter.services';
+import { getDorms, postReviewForDorm, putSavedDorm } from '../services/renter.services';
 import { validateRequiredFields, validateReviewCommentLength } from '../utils/input.validators';
 
 export const getDormsController = async (req: Request & { user?: UserType }, res: Response) => {
@@ -36,6 +36,27 @@ export const postReviewController = async (req: Request & { user?: UserType }, r
             dorm_id,
             stars,
             comment
+        );
+
+        if (dorm_post_update.httpCode === 200) return res.status(dorm_post_update.httpCode).json({ 'message': dorm_post_update.message });
+
+        return res.status(dorm_post_update.httpCode).json({ 'error': dorm_post_update.error });
+    } catch (error) {
+        return res.status(500).json({ 'error': 'Internal Server Error' });
+    }
+}
+
+export const putSavedDormController = async (req: Request & { user?: UserType }, res: Response) => {
+    try {
+        const user = req.user;
+        if (!user) return res.status(404).json({ error: "User not found." });
+
+        const { dorm_id } = req.params;
+        if (!dorm_id) return res.status(404).json({ error: "Dorm not found." });
+
+        const dorm_post_update = await putSavedDorm(
+            user.user_id,
+            dorm_id,
         );
 
         if (dorm_post_update.httpCode === 200) return res.status(dorm_post_update.httpCode).json({ 'message': dorm_post_update.message });
