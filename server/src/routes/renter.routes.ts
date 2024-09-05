6cdecf5102206b56f08e59f5b4e85433064a997e
swa-ne <spaulbautista@gmail.com>
2024-09-05 23:28:26 +0800

    Add endpoint to save dorms for users.

--- server/src/routes/renter.routes.ts
+++ server/src/routes/renter.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/token.authentication";
-import { getDormsController, postReviewController } from "../controllers/renter.controller";
+import { getDormsController, postReviewController, putSavedDormController } from "../controllers/renter.controller";
 
 const router = Router();
 
 router.use(authenticateToken);
 
 router.get("/my-dorms", getDormsController);
-router.post("/give-review/:dorm_id", postReviewController)
+router.post("/give-review/:dorm_id", postReviewController);
+router.put("/add/saved", putSavedDormController);
 
 export default router;
