1b3187b263fe4e4cf219ab340ca89122d30d88e4
Swa-ne <spaulbautista@gmail.com>
2024-09-06 22:57:32 +0800

    Created an endpoint in renter.routes.ts to include the DELETE route /remove/saved.

--- server/src/routes/renter.routes.ts
+++ server/src/routes/renter.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/token.authentication";
-import { getDormsController, postReviewController, putSavedDormController } from "../controllers/renter.controller";
+import { deleteSavedDormController, getDormsController, postReviewController, putSavedDormController } from "../controllers/renter.controller";
 
 const router = Router();
 
@@ -9,5 +9,6 @@ router.use(authenticateToken);
 router.get("/my-dorms", getDormsController);
 router.post("/give-review/:dorm_id", postReviewController);
 router.put("/add/saved", putSavedDormController);
+router.delete("/remove/saved", deleteSavedDormController);
 
 export default router;
