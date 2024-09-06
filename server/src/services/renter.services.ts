40548d09f92d7f675e018868fc4d62af04bfb7cb
Swa-ne <spaulbautista@gmail.com>
2024-09-06 22:58:26 +0800

    Implement service function for deleting saved dorms.

--- server/src/services/renter.services.ts
+++ server/src/services/renter.services.ts
@@ -43,7 +43,11 @@ export const putSavedDorm = async (user_id: string, dorm_id: string) => {
         const saved = await Saved.findOne({ user_id }).session(session);
         const dorm = await Dorm.findById(dorm_id).session(session);
 
-        if (!dorm) return { error: 'Dorm not found', httpCode: 404 };
+        if (!dorm) {
+            await session.abortTransaction();
+            session.endSession();
+            return { error: 'Dorm not found', httpCode: 404 };
+        }
 
         if (saved !== null) {
             saved.dorm_ids.push(dorm._id);
@@ -53,6 +57,48 @@ export const putSavedDorm = async (user_id: string, dorm_id: string) => {
                 user_id,
                 dorm_ids: [dorm_id],
             }).save({ session });
+        }
+
+        await session.commitTransaction();
+        session.endSession();
+
+        return { message: 'Success', httpCode: 200 };
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        return { error: 'Internal Server Error', httpCode: 500 };
+    }
+}
+
+export const deleteSavedDorm = async (user_id: string, dorm_id: string) => {
+    const session = await startSession();
+    session.startTransaction();
+
+    try {
+        const saved = await Saved.findOne({ user_id }).session(session);
+        const dorm = await Dorm.findById(dorm_id).session(session);
+
+        if (!dorm) {
+            await session.abortTransaction();
+            session.endSession();
+            return { error: 'Dorm not found', httpCode: 404 };
+        }
+
+        if (saved) {
+            const dorm_index = saved.dorm_ids.indexOf(dorm._id);
+
+            if (dorm_index > -1) {
+                saved.dorm_ids.splice(dorm_index, 1);
+                await saved.save({ session });
+            } else {
+                await session.abortTransaction();
+                session.endSession();
+                return { error: 'Dorm not found in saved list', httpCode: 404 };
+            }
+        } else {
+            await session.abortTransaction();
+            session.endSession();
+            return { error: 'Saved list not found for this user', httpCode: 404 };
         }
 
         await session.commitTransaction();
