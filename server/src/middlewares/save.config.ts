64696236f77e2c730151a3d01e4fb78a1c60605b
swa-ne <spaulbautista@gmail.com>
2024-09-08 22:51:50 +0800

    Enhance file upload with image filtering and separate multer configurations.

--- server/src/middlewares/save.config.ts
+++ server/src/middlewares/save.config.ts
@@ -1,9 +1,21 @@
-import multer from 'multer';
+import multer, { FileFilterCallback } from 'multer';
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 import config from '../config/firebase.config';
+import { Request } from 'express';
 
 initializeApp(config.firebaseConfig);
 export const storage = getStorage();
-export const upload = multer({ storage: multer.memoryStorage() });
+
+const imageFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+    if (file.mimetype.startsWith("image/")) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only image files are allowed"));
+    }
+};
+
+export const uploadDorm = multer({ storage: multer.memoryStorage(), fileFilter: imageFilter });
+
+export const uploadMessage = multer({ storage: multer.memoryStorage() });
