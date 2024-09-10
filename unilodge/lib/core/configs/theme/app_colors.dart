a3c23feb8aa233d045b99f17f57c24fee9b87a19
swa-ne <spaulbautista@gmail.com>
2024-09-11 02:08:20 +0800

    correct hex value of blueTextColor

--- unilodge/lib/core/configs/theme/app_colors.dart
+++ unilodge/lib/core/configs/theme/app_colors.dart
@@ -6,7 +6,7 @@ class AppColors {
   static const darkBackground = Color(0xFF161616);
   static const textColor = Color(0xff454545);
   static const lightBlueTextColor = Color(0xff6F808D);
-  static const blueTextColor = Color(0xff2D4252);
+  static const blueTextColor = Color(0x0D2D4252);
   static const favoriteButton = Color(0xffF04F43);
   static const linearGreen = Color(0xff83A2AC);
   static const linearOrange = Color(0xffFB7878);
