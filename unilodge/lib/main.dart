9f71d3f6f8ef5c06c1112a2b9ce880f22d152c7b
swa-ne <spaulbautista@gmail.com>
2024-09-12 23:21:10 +0800

    Added SafeArea widget to MainApp to ensure content is displayed within safe boundaries on different screens.

--- unilodge/lib/main.dart
+++ unilodge/lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:unilodge/core/configs/theme/app_theme.dart';
-import 'app_routes.dart'; 
+import 'app_routes.dart';
 
 void main() {
   runApp(const MainApp());
 }
-
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      title: 'UniLodge',
-      routerConfig: appRouter,
+    return SafeArea(
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        title: 'UniLodge',
+        routerConfig: appRouter,
+      ),
     );
   }
 }
