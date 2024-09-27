37568d265ff48bde8521fe79253a6fe1c9a185ac
swa-ne <spaulbautista@gmail.com>
2024-09-28 00:52:27 +0800

    Enhance authentication: Added Google sign-in and logout methods; updated Android signing configuration.

--- unilodge/lib/data/sources/auth/authFirebaseService.dart
+++ unilodge/lib/data/sources/auth/authFirebaseService.dart
@@ -1,23 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:unilodge/data/models/user.dart';
 
 abstract class AuthFirebaseService {
   Future<GoogleSignInAccount?> signInWithGoogle();
+  Future<GoogleSignInAccount?> logoutWithGoogle();
 }
 
 class AuthFirebaseServiceImpl extends AuthFirebaseService {
+  static final _googleSignIn = GoogleSignIn();
   @override
   Future<GoogleSignInAccount?> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
-
-    // final credential = googleAuth.credential(
-    //   accessToken: googleAuth?.accessToken,
-    //   idToken: googleAuth?.idToken,
-    // );
-
-    // return await FirebaseAuth.instance.signInWithCredential(credential);
+    if (googleUser == null) {
+      // TODO: SHOW SIGN IN FAILED
+      print("Login Failed");
+      return null;
+    }
+    return googleUser;
   }
+
+  @override
+  Future<GoogleSignInAccount?> logoutWithGoogle() async =>
+      await _googleSignIn.disconnect();
 }
