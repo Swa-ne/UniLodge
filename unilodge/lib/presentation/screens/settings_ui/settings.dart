1925802672aee28966460a225ed2042e82ac6af2
Stephen Bautista <stba.bautista.up@phinmaed.com>
2024-10-07 15:32:11 +0800

    Modified _showLogoutConfirmation method to trigger LogOutEvent via AuthBloc when the user confirms logoutand updated LogoutConfirmBottomSheet to invoke the logout callback, handling user confirmation.

--- unilodge/lib/presentation/screens/settings_ui/settings.dart
+++ unilodge/lib/presentation/screens/settings_ui/settings.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:unilodge/bloc/authentication/auth_bloc.dart';
+import 'package:unilodge/bloc/authentication/auth_event.dart';
+import 'package:unilodge/bloc/authentication/auth_state.dart';
 import 'package:unilodge/common/widgets/custom_text.dart';
 import 'package:unilodge/core/configs/theme/app_colors.dart';
 import 'package:unilodge/presentation/widgets/settings_widg/logout_confirm_bottom_sheet.dart';
@@ -9,76 +13,88 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, color: AppColors.textColor),
-          onPressed: () {
-            context.go('/user-profile');
-          },
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is LogOutSuccess) {
+          context.go("/account-selection-login");
+        } else if (state is LogOutError) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(state.error)),
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios, color: AppColors.textColor),
+            onPressed: () {
+              context.go('/user-profile');
+            },
+          ),
+          title: const CustomText(
+            text: 'Settings',
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          centerTitle: true,
         ),
-        title: const CustomText(
-          text: 'Settings',
-          color: Colors.black,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ListTile(
-              leading: const Icon(Icons.person, color: AppColors.textColor),
-              title: const CustomText(
-                text: 'My Profile',
-                fontSize: 16,
-                color: AppColors.textColor,
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.person, color: AppColors.textColor),
+                title: const CustomText(
+                  text: 'My Profile',
+                  fontSize: 16,
+                  color: AppColors.textColor,
+                ),
+                onTap: () {
+                  context.go('/my-profile');
+                },
               ),
-              onTap: () {
-                context.go('/my-profile');
-              },
-            ),
-            const Divider(),
-            ListTile(
-              leading: const Icon(Icons.lock, color: AppColors.textColor),
-              title: const CustomText(
-                text: 'Change Password',
-                fontSize: 16,
-                color: AppColors.textColor,
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.lock, color: AppColors.textColor),
+                title: const CustomText(
+                  text: 'Change Password',
+                  fontSize: 16,
+                  color: AppColors.textColor,
+                ),
+                onTap: () {
+                  context.go('/my-profile');
+                },
               ),
-              onTap: () {
-                context.go('/my-profile');
-              },
-            ),
-            const Divider(),
-            ListTile(
-              leading: const Icon(Icons.logout, color: AppColors.textColor),
-              title: const CustomText(
-                text: 'Logout',
-                fontSize: 16,
-                color: AppColors.textColor,
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.logout, color: AppColors.textColor),
+                title: const CustomText(
+                  text: 'Logout',
+                  fontSize: 16,
+                  color: AppColors.textColor,
+                ),
+                onTap: () {
+                  _showLogoutConfirmation(context);
+                },
               ),
-              onTap: () {
-                _showLogoutConfirmation(context);
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
   void _showLogoutConfirmation(BuildContext context) {
+    final _authBloc = BlocProvider.of<AuthBloc>(context);
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
         return LogoutConfirmBottomSheet(
           onLogout: () {
-            //
+            _authBloc.add(LogOutEvent());
           },
         );
       },
