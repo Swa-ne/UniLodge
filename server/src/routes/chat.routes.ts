8fc59a20fb62ddeda2104c93adf70ec6aa09d089
Stephen Bautista <stba.bautista.up@phinmaed.com>
2024-10-08 17:22:34 +0800

    Added chat services, controllers, and routes for managing user status, inbox creation, message handling, and fetching active users. Implemented token-based authentication for secure access to chat-related functionalities.

---
+++ server/src/routes/chat.routes.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response, Router } from "express"
+import { createPrivateInboxController, getAllActiveUsersController, getChatMessagesController, getInboxController, getInboxDetailsController, getMessagesController, getUserDetailsController, saveMessageController } from "../controllers/chat.controller"
+import { authenticateToken } from "../middlewares/token.authentication"
+
+
+const router = Router()
+
+router.get("/open", getChatMessagesController)
+router.post("/create_private_inbox", authenticateToken, createPrivateInboxController)
+router.get("/get_inbox_details", authenticateToken, getInboxDetailsController)
+router.post("/save_message", authenticateToken, saveMessageController)
+router.get("/get_messages", authenticateToken, getMessagesController)
+router.get("/get_user_details", authenticateToken, getUserDetailsController)
+router.get("/get_activesers", authenticateToken, getAllActiveUsersController)
+router.get("/get_inbox", authenticateToken, getInboxController)
+
+export default router
