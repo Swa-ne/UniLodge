921c263925cdc207a0803eaed4b7f4d8da1e4686
Stephen Bautista <stba.bautista.up@phinmaed.com>
2024-10-09 17:48:51 +0800

    Added Firebase storage image deletion functionality with deleteImage method.

--- server/src/middlewares/save.config.ts
+++ server/src/middlewares/save.config.ts
@@ -1,6 +1,6 @@
 import multer, { FileFilterCallback } from 'multer';
 import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
+import { deleteObject, getStorage, ref } from 'firebase/storage';
 
 import config from '../config/firebase.config';
 import { Request } from 'express';
@@ -15,6 +15,31 @@ const imageFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCall
         cb(new Error("Only image files are allowed"));
     }
 };
+
+export const deleteImage = async (filePath: string) => {
+    try {
+        const url = extractFilePath(filePath);
+        if (!url) {
+            throw "File not found!";
+        }
+        const fileRef = ref(storage, url);
+
+        await deleteObject(fileRef);
+    } catch (error) {
+        throw error;
+    }
+};
+
+function extractFilePath(url: string): string | null {
+    const regex = /\/o\/(.*?)\?alt=media/;
+    const match = url.match(regex);
+
+    if (match && match[1]) {
+        return decodeURIComponent(match[1]);
+    }
+
+    return null;
+}
 
 export const uploadDorm = multer({ storage: multer.memoryStorage(), fileFilter: imageFilter });
 
