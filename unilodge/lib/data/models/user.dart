026a6051c7694c1b913ff63af508f125d34d1356
Stephen Bautista <stba.bautista.up@phinmaed.com>
2024-10-11 01:38:03 +0800

    Refactor UserModel to use Equatable for value comparison and update JSON methods.

--- unilodge/lib/data/models/user.dart
+++ unilodge/lib/data/models/user.dart
@@ -1,15 +1,35 @@
-class UserModel {
-  String? id;
-  String? full_name;
-  String? email;
-  String? profile;
+import 'package:equatable/equatable.dart';
 
-  UserModel({this.id, this.full_name, this.email, this.profile});
+class UserModel extends Equatable {
+  final String id;
+  final String full_name;
+  final String email;
+  final String profile;
 
-  UserModel.fromJson(Map<String, dynamic> data) {
-    id = data['_id'];
-    full_name = data['full_name'];
-    email = data['personal_email'];
-    profile = data['profile'];
+  const UserModel({
+    required this.id,
+    required this.full_name,
+    required this.email,
+    required this.profile,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> data) {
+    return UserModel(
+      id: data['_id'],
+      full_name: data['full_name'],
+      email: data['personal_email'],
+      profile: data['profile_picture_url'],
+    );
   }
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'full_name': full_name,
+      'personal_email': email,
+      'profile_picture_url': profile,
+    };
+  }
+
+  @override
+  List<Object> get props => [id, full_name, email, profile];
 }
