d9f5f589aa06e844d89c62d6f60ee3cf2bef3b29
jairasolis <jairasolis030@gmail.com>
2024-10-18 01:46:57 +0800

    added initital route for payment page

--- unilodge/lib/app_routes.dart
+++ unilodge/lib/app_routes.dart
@@ -6,7 +6,7 @@ import 'package:unilodge/presentation/screens/admin/admin_listings_screen.dart';
 import 'package:unilodge/presentation/screens/admin/status_listing_screen.dart';
 import 'package:unilodge/presentation/screens/auth/change_forgotten_password.dart';
 import 'package:unilodge/presentation/screens/auth/change_password.dart';
-import 'package:unilodge/presentation/screens/book/booked_listings.dart';
+import 'package:unilodge/presentation/screens/crypto/payment_page.dart';
 import 'package:unilodge/presentation/screens/help_center/help_center.dart';
 import 'package:unilodge/presentation/screens/auth/account_selection_login.dart';
 import 'package:unilodge/presentation/screens/auth/account_selection_sign_up.dart';
@@ -205,7 +205,7 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/bookings',
       builder: (context, state) {
-        return const BookedListings();
+        return const PaymentPage(); // TODO: 
       },
     ),
     GoRoute(
