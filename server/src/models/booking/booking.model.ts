3e25ccfd2270659217c33f8064a5c61f278bd5fb
jairasolis <jairasolis030@gmail.com>
2024-10-23 01:22:16 +0800

    added paid status on the booking model

--- server/src/models/booking/booking.model.ts
+++ server/src/models/booking/booking.model.ts
@@ -1,45 +1,45 @@
 import { Document, model, ObjectId, Schema } from "mongoose";
 
 export interface BookingSchemaInterface extends Document {
-    _id: ObjectId;
-    user_id: ObjectId;
-    listing_id: ObjectId;
-    propertyType: string;
-    price: number;
-    status: string;
-    createdAt?: Date;
-    updatedAt?: Date;
+  _id: ObjectId;
+  user_id: ObjectId;
+  listing_id: ObjectId;
+  propertyType: string;
+  price: number;
+  status: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const BookingSchema = new Schema(
-    {
-        user_id: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-            required: true,
-        },
-        listing_id: {
-            type: Schema.Types.ObjectId,
-            ref: 'Dorm',
-            required: true
-        },
-        propertyType: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        status: {
-            type: String,
-            enum: ['Pending', 'Accepted', 'Rejected'],
-            default: 'Pending',
-        },
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    listing_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Dorm",
+      required: true,
+    },
+    propertyType: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Accepted", "Rejected", "Paid"],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export const Booking = model<BookingSchemaInterface>("Booking", BookingSchema);
